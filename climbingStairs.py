70975d4b65aedfd8382d7d8a39c84c285a24a3ce
satyam06050 <krsatyam0605@gmail.com>
2026-08-12 18:15:17 +0530

    add climbing stairs

---
+++ climbingStairs.py
@@ -0,0 +1,32 @@
+# Problem Statement: Climbing Stairs
+
+# Alice has a staircase with N steps and wants to reach the top. In each move, she can climb either:
+
+# 1 step, or
+# M steps
+
+# Your task is to determine the minimum number of moves required for Alice to reach exactly the Nth step.
+
+# Input Format
+
+# The input consists of a single line containing two space-separated integers:
+
+# N M
+
+# Output Format
+
+# Print the minimum number of moves required to reach exactly the Nth step.
+
+# Constraints
+# 1 ≤ N, M ≤ 10⁹
+
+# Example:
+
+# Input:
+# 10 3
+
+# Output:
+# 4
+
+n,m=map(int,input().split())
+print(n,m)
